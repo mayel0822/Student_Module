74248207a039d6ff5c979397b5e451d0ccbee030
John Carlo <bravo.287104@sjdelmonte.sti.edu.ph>
2026-06-21 18:31:12 +0800

    Initial commit - Student/Dashboard module frontend

---
+++ Pages/ComputerVacancy.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookHiveDashBoard.Pages
+{
+    public class ComputerVacancyModel : PageModel
+    {
+        public void OnGet() { }
+    }
+}
